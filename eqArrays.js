b233bb0fa58dbb746a02c1fc236cb4c1b8d26d87
Dicky jr <lmayhenderson@yahoo.com>
2024-03-05 21:23:30 -0800

    added a function to compare arrays + test cases

---
+++ eqArrays.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+}
+return true
+}
+
+console.log(assertEqual(eqArrays([4,5,6],[4,5,6]), true))
+console.log(assertEqual(eqArrays(["lighthouse", "labs"],["lighthouse", "labs"]), true))
+console.log(assertEqual(eqArrays(["lighthouse", "labs"],["lighthouse", "labs", "test-case"]), false))
