5cf006a9cad2d91e03f3104dcf66ea6e97b367d9
Dicky jr <lmayhenderson@yahoo.com>
2024-03-05 22:21:38 -0800

    updated

---
+++ without.js
@@ -0,0 +1,25 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+}
+return true
+}
+
+
+const assertArraysEqual = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+      return
+    }
+}
+console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`)
+}
+
+console.log(assertArraysEqual(["hi", "hi"], ["hi", "hi"]));
+console.log(assertArraysEqual(["hi", "hi"], ["hi", "hello"]));
