1d32238d053da312796e356c4c911321bcba4d9e
Dicky jr <lmayhenderson@yahoo.com>
2024-03-05 21:59:38 -0800

    updated

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,14 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
   }
-};
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+}
+return true
+}
 
 
 const assertArraysEqual = function(arr1, arr2) {
@@ -21,5 +25,5 @@ const assertArraysEqual = function(arr1, arr2) {
 console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`)
 }
 
-console.log(assertArraysEqual(["hi", "hi"], ["hi", "hi"]))
-console.log(assertArraysEqual(["hi", "hi"], ["hi", "hello"]))
+console.log(assertArraysEqual(eqArrays(["hi", "hi"], ["hi", "hi"])))
+console.log(assertArraysEqual(eqArrays(["hi", "hi"], ["hi", "hello"])))
