a7160d9565d7bbe3b660451b6467ca1f6b1a836d
Dicky jr <lmayhenderson@yahoo.com>
2024-03-06 19:26:26 -0800

    letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,40 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    } 
+}
+return true
+}
+
+
+const letterPositions = function(sentence) {
+  const results = {};
+
+  for (let i = 0; i < sentence.length; i++) {
+    const letter = sentence[i]
+    if (results[letter] != '') {
+    if (results[letter]) {
+      results[letter].push(i)
+    } else {
+      results[letter] = [i]
+    }
+  }
+  }
+  return results;
+};
+
+
+console.log(assertArraysEqual(letterPositions("hello").e, [1]));
+console.log(assertArraysEqual(letterPositions("goodbye").e, [1]));
