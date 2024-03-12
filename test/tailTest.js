fd0a9bbf2c5dcb208cf038feb262308b1ed3bdb0
Dicky jr <lmayhenderson@yahoo.com>
2024-03-11 21:24:29 -0700

    fixed linting errors

--- test/tailTest.js
+++ test/tailTest.js
@@ -6,8 +6,8 @@ const { assert } = require('chai');
 describe('#tail', () => {
 	it('should return [] for [1]', () => {
 		assert.deepEqual(tail([1]), [])
-	})
+	});
 	it('should return [2, 3] for [1, 2, 3]', () => {
 		assert.deepEqual(tail([1, 2, 3]), [2, 3])
-	})
+	});
 })
