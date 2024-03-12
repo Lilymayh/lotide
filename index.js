5c4fa7194d91c65fe72fe98f07b6db796fedc9a9
Dicky jr <lmayhenderson@yahoo.com>
2024-03-11 21:52:07 -0700

    added export object for all fucntions

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const head   = require('./head')
 const middle = require('./middle')
 const tail   = require('./tail')
+const assertArraysEqual  = require('./assertArraysEqual')
+const  assertEqual = require('./assertEqual')
+const countLetters = require('./countLetters')
+const  countOnly = require('./countOnly')
+const eqArrays  = require('./eqArrays')
+const eqObjects = require('./eqObjects')
+const  findKey = require('./findKey')
+const  findKeyByValue = require('./findKeyByValue')
+const letterPositions  = require('./letterPositions')
+const map  = require('./map')
+const takeUntil  = require('./takeUntil')
+const without = require('./without')
+
+
 
 module.exports = {
 	head: head,
 	tail: tail,
-	middle: middle
+	middle: middle,
+	assertArraysEqual: assertArraysEqual,
+	assertEqual: assertEqual,
+	countLetters: countLetters,
+	countOnly: countOnly,
+	eqArrays: eqArrays,
+	eqObjects: eqObjects,
+	findKey: findKey,
+	findKeyByValue: findKeyByValue,
+	letterPositions: letterPositions,
+	map: map,
+	takeUntil: takeUntil,
+	without: without
 }
--- test/tailTest.js
+++ test/tailTest.js
@@ -3,9 +3,15 @@ const { assert } = require('chai');
 
 
 describe('#tail', () => {
+	it('should return [] for []', () => {
+		assert.deepEqual(tail([]), [])
+	})
 	it('should return [] for [1]', () => {
 		assert.deepEqual(tail([1]), [])
 	});
+	it('should return [2] for [1, 2]', () => {
+		assert.deepEqual(tail([1, 2]), [2])
+	})
 	it('should return [2, 3] for [1, 2, 3]', () => {
 		assert.deepEqual(tail([1, 2, 3]), [2, 3])
 	});
